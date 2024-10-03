4fceaf14ff68a2394ecb7ae0180478159a579662
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-03 16:06:08 -0300

    feat: add link to product

--- src/app/(main)/product.tsx
+++ src/app/(main)/product.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Product as ProductType } from '../../lib/product/types'
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface ProductProps {
   product: ProductType
@@ -8,35 +9,37 @@ interface ProductProps {
 
 const Product = ({ product }: ProductProps) => {
   return (
-    <article className="border rounded-md overflow-hidden h-full">
-      <Image
-        src={product.image}
-        alt={product.title}
-        width={400}
-        height={480}
-        className="object-cover aspect-[1/1.2]"
-      />
-      <div className="px-3 py-2">
-        <h3
-          title={product.title}
-          style={{
-            WebkitLineClamp: 2,
-            WebkitBoxOrient: 'vertical',
-            display: '-webkit-box',
-          }}
-          className="max-h-8 leading-4 text-sm text-ellipsis overflow-hidden"
-        >
-          {product.title}
-        </h3>
-        <span className="font-bold text-3xl">
-          <span className="sr-only">Price</span>
-          {product.price.toLocaleString('pt', {
-            style: 'currency',
-            currency: 'BRL',
-          })}
-        </span>
-      </div>
-    </article>
+    <Link href={`/product/${product.id}`} className="group">
+      <article className="border rounded-md overflow-hidden h-full">
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={400}
+          height={480}
+          className="object-cover aspect-[1/1.2]"
+        />
+        <div className="px-3 py-2">
+          <h3
+            title={product.title}
+            style={{
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: 'vertical',
+              display: '-webkit-box',
+            }}
+            className="max-h-8 leading-4 text-sm text-ellipsis overflow-hidden hover:underline group-hover:underline"
+          >
+            {product.title}
+          </h3>
+          <span className="font-bold text-3xl">
+            <span className="sr-only">Price</span>
+            {product.price.toLocaleString('pt', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </span>
+        </div>
+      </article>
+    </Link>
   )
 }
 
