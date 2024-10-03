c773b489f1658eb08baa0ba4662f9a8fb041249f
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-03 13:21:41 -0300

    feat: nextjs image config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['fakestoreapi.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+      },
+    ],
   },
 }
 
