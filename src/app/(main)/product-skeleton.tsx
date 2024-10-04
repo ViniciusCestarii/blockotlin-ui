2b34dd480da6161a08098717b7b15d223dfdde86
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-04 11:45:01 -0300

    feat: improve skeleton

--- src/app/(main)/product-skeleton.tsx
+++ src/app/(main)/product-skeleton.tsx
@@ -11,7 +11,7 @@ const randomWidht = () => {
 }
 
 const ProductSkeleton = ({ className, ...props }: ProductSkeletonProps) => (
-  <div {...props} className={cn('space-y-2 pb-2', className)}>
+  <div {...props} className={cn('space-y-2 pb-3', className)}>
     <Skeleton className="w-full aspect-[1/1.2] px-2 rounded-b-none" />
     <Skeleton className="w-full h-8" />
     <Skeleton className={cn('w-full h-6', randomWidht())} />
